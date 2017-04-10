7512cbe59656f58dba295687d443064177ae3cba
edo_m18 <edo.m18@gmail.com>
2017-04-11 01:54:15 +0900

    Added leader controller.

---
+++ Assets/Flocking/Scripts/LeaderController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderController : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 0.3f;
+
+	void Update()
+    {
+        bool isShift = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right * _speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += transform.right * _speed;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (isShift)
+            {
+                transform.position += transform.up * _speed;
+            }
+            else
+            {
+                transform.position += transform.forward * _speed;
+            }
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (isShift)
+            {
+                transform.position -= transform.up * _speed;
+            }
+            else
+            {
+                transform.position -= transform.forward * _speed;
+            }
+        }
+	}
+}
